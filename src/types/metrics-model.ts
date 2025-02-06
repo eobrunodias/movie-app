cbffa0bcfff087906c17711efdf4d45786141eab
Bruno <brunodias40@gmail.com>
2025-02-06 13:26:04 -0300

    feat: add MetricsModel interface

---
+++ src/types/metrics-model.ts
@@ -0,0 +1,12 @@
+export interface MetricsModel {
+  searchTerm: string;
+  count: number;
+  poster_path: string;
+  movie_id: number;
+  $id: string;
+  $createdAt: string;
+  $updatedAt: string;
+  $permissions: unknown[];
+  $databaseId: string;
+  $collectionId: string;
+}
