e7ea586bdab0e064fbcc67fa134e3ccc7e2d9aa9
Bruno <brunodias40@gmail.com>
2025-02-06 17:10:06 -0300

    feat: add getTrendingMovies function to retrieve top trending movies based on search count

--- src/api/appwrite.ts
+++ src/api/appwrite.ts
@@ -2,6 +2,7 @@ import { Client, Databases, ID, Query } from "appwrite";
 import { COLLECTION_ID, DATABASE_ID, PROJECT_ID } from "../constants/database";
 import { Results } from "../types/movie";
 import { API_BASE_IMAGE_URL } from "../constants/api";
+import { MetricsModel } from "../types/metrics-model";
 
 const client = new Client();
 
@@ -9,14 +10,15 @@ client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
 
 const database = new Databases(client);
 
-export async function updateSearchCount(searchTerm: string, movie: Results) {
+export async function updateSearchCount(
+  searchTerm: string,
+  movie: Results
+): Promise<void> {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
       Query.equal("searchTerm", searchTerm),
     ]);
 
-    console.log("result.documents: ", result.documents);
-    console.log("result: ", result);
     if (result.documents.length > 0) {
       const doc = result.documents[0];
 
@@ -31,6 +33,18 @@ export async function updateSearchCount(searchTerm: string, movie: Results) {
         poster_path: `${API_BASE_IMAGE_URL}${movie.poster_path}`,
       });
     }
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
+export async function getTrendingMovies(): Promise<MetricsModel[] | undefined> {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    return result.documents as MetricsModel[];
   } catch (error) {
     console.error(error);
   }
