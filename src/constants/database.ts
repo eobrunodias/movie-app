0ef5fd5d33562c69cfe4029ba4eeabba55942f4f
Bruno <brunodias40@gmail.com>
2025-02-06 11:47:02 -0300

    feat: add Appwrite configuration constants for project, database, and collection IDs

---
+++ src/constants/database.ts
@@ -0,0 +1,6 @@
+export const PROJECT_ID: string = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+
+export const DATABASE_ID: string = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+
+export const COLLECTION_ID: string = import.meta.env
+  .VITE_APPWRITE_COLLECTION_ID;
