c3881c2c5bab8abeba3d5dddbbb7827ad7da4f83
Bruno <brunodias40@gmail.com>
2025-02-13 19:04:48 -0300

    fix: update API_BASE_URL and API_KEY to use environment variables

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL: string = "https://newsapi.org/v2";
+export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL;
 
-export const API_KEY: string = import.meta.env.VITE_NEWSAPI_KEY;
+export const API_KEY: string = import.meta.env.VITE_API_KEY;
