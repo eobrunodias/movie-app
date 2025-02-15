86d2bb99b5f8a67178563b68fecaad96aff5ea95
Bruno <brunodias40@gmail.com>
2025-02-15 18:42:37 -0300

    feat: add Modal component and refactor Search component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,59 @@
+import { XCircleIcon } from "lucide-react";
+import { cleanContent } from "../../utils/cleanContent";
+
+interface ModalProps {
+  isOpen: boolean;
+  newsData: {
+    title: string;
+    content?: string;
+    url?: string;
+    image?: string;
+  };
+  onClose: () => void;
+}
+
+export default function Modal({ isOpen, newsData, onClose }: ModalProps) {
+  if (!isOpen || !newsData) return null;
+
+  return (
+    <div className="fixed inset-0 bg-bg-modal bg-opacity-50 flex items-center justify-center ">
+      <div className="bg-bg-modal text-white rounded-lg w-3/4 sm:w-1/2 max-h-[80vh] overflow-hidden relative border-1 border-borderwhite">
+        <div className="rounded-full bg-gray-900 w-13 h-13 absolute top-5 right-5 z-10 flex items-center justify-center">
+          <XCircleIcon
+            onClick={onClose}
+            className="text-red cursor-pointer"
+            size="40px"
+          />
+        </div>
+
+        <div className="relative mb-12">
+          <img
+            src={newsData.image || "https://placehold.co/600x400"}
+            alt="poster"
+            className="object-cover w-full h-64 rounded-t-lg"
+          />
+        </div>
+
+        <div className="overflow-y-auto max-h-[50vh]">
+          <div className="px-6 flex flex-col justify-between h-full mb-12">
+            <h1 className="text-5xl font-bold mb-12">{newsData.title}</h1>
+
+            <div className="max-h-[calc(100%-16rem)] mb-12">
+              <p className="text-2xl ">
+                {cleanContent(newsData.content || "")}
+              </p>
+            </div>
+
+            <a
+              href={newsData.url}
+              className="text-blue-500 font-bold text-1xl cursor-pointer"
+            >
+              <span className="text-colorfontbutton italic">Source: </span>
+              {newsData.url}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,20 +1,11 @@
-interface SearchProps {
-  searchTerm: string;
-  setSearchTerm: (value: string) => void;
-}
+import { SearchIcon } from "lucide-react";
 
-export default function Search({ searchTerm, setSearchTerm }: SearchProps) {
+export default function Search() {
   return (
-    <div className="search">
-      <div>
-        <img src="search.svg" alt="search" />
-
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search through thousand of movies"
-        />
+    <div className="bg-gray-600 border-1 p-4 border-borderwhite flex gap-5 text-[20px]  text-white rounded-[10px] min-w-[266px] max-h-[58px] items-center cursor-pointer">
+      <div className="flex">
+        <SearchIcon className="mr-4" />
+        <input type="text" className="outline-none max-w-[185px]" />
       </div>
     </div>
   );
