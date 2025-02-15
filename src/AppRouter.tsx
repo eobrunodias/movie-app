5b8b83e50f6e12c61887d5035057dbf0a8243776
Bruno <brunodias40@gmail.com>
2025-02-15 18:39:16 -0300

    feat: add AppRouter component

---
+++ src/AppRouter.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Trending from "./pages/Trending";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Home className="px-[50px] overflow-y-auto pt-[24px] mb-[24px]" />
+        }
+      />
+      <Route
+        path="/favorites"
+        element={
+          <Favorites className="px-[50px] overflow-y-auto pt-[24px] mb-[24px]" />
+        }
+      ></Route>
+      <Route
+        path="/trending"
+        element={
+          <Trending className="px-[50px] overflow-y-auto pt-[24px] mb-[24px]" />
+        }
+      ></Route>
+    </Routes>
+  );
+}
