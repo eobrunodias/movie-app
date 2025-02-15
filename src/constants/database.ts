6867589926d60dbf0292f17181888b4309f9e0d6
Bruno <brunodias40@gmail.com>
2025-02-15 18:36:55 -0300

    feat: add new axios API functions for fetching news and update database constants

--- src/constants/database.ts
+++ src/constants/database.ts
@@ -2,5 +2,8 @@ export const PROJECT_ID: string = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
 export const DATABASE_ID: string = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 
-export const COLLECTION_ID: string = import.meta.env
-  .VITE_APPWRITE_COLLECTION_ID;
+export const ARTICLES_COLLECTION_ID: string = import.meta.env
+  .VITE_APPWRITE_ARTICLES_COLLECTION_ID;
+
+export const CONFIG_COLLECTION_ID: string = import.meta.env
+  .VITE_APPWRITE_CONFIG_COLLECTION_ID;
