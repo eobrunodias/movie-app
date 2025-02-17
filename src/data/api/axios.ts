21762777dd6e8afdeec8b05ff5cfdba5269cb606
Bruno <brunodias40@gmail.com>
2025-02-17 20:31:54 -0300

    feat: refactor API calls and add Appwrite client

--- src/data/api/axios.ts
+++ src/data/api/axios.ts
@@ -1,22 +1,10 @@
 import axios from "axios";
-import { API_BASE_URL, API_KEY } from "../../constants/api";
 import { ApiResponse } from "../../types/api";
 import { FilteredNews } from "../../types/appwrite";
 
-interface AxiosConfigProps {
-  endpoint: string;
-}
-
-export async function fetchApi({ endpoint }: AxiosConfigProps) {
-  const response: ApiResponse = await axios.get(`${API_BASE_URL}${endpoint}`);
-  return response;
-}
-
 export async function fetchNews(): Promise<FilteredNews[] | undefined> {
   try {
-    const response: ApiResponse = await fetchApi({
-      endpoint: `/search?q=example&lang=en&country=us&max=10&apikey=${API_KEY}`,
-    });
+    const response: ApiResponse = await axios.get("/news");
 
     const articles: FilteredNews[] = response.data.articles;
 
