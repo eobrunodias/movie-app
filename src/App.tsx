f6c3fb7fa07ac0a85d0964c67c86bb322311d3f5
Bruno <brunodias40@gmail.com>
2025-02-17 22:55:29 -0300

    feat: update routing with HashRouter and adjust Vercel configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Sidebar from "./components/Sidebar";
 import AppRoutes from "./AppRouter";
 import { SearchProvider } from "./context/search/SearchProvider";
+import { HashRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <SearchProvider>
       <main className="flex flex-1 h-screen overflow-hidden sm:flex-row flex-col">
         <Sidebar />
-        <AppRoutes />
+        <HashRouter>
+          <AppRoutes />
+        </HashRouter>
       </main>
     </SearchProvider>
   );
